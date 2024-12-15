c4d0f980677f8775000c014f16baadb90a5a7eff
Derek Zhang <chromestone@users.noreply.github.com>
2024-12-15 13:47:53 -0500

    updated RealScoreEnv to use z score
    and update test_env

--- datingrl/env.py
+++ datingrl/env.py
@@ -1,5 +1,7 @@
 """
-TODO
+env.py
+
+This module contains our custom Gymnasium environments.
 """
 
 from typing import Any, Optional
@@ -9,7 +11,7 @@ import numpy as np
 
 class RealScoreEnv(gym.Env):
     """
-    This environment only outputs the index with the real score as the observation.
+    This environment only outputs the index with the z-score as the observation.
     """
 
     def __init__(self, env_config: dict[str, Any]):
@@ -22,50 +24,65 @@ class RealScoreEnv(gym.Env):
         self.observation_space = gym.spaces.Box(
             low=  np.array([0.0, -np.inf], dtype=np.float32),
             high= np.array([1.0,  np.inf], dtype=np.float32),
-            dtype=np.float32)
+            dtype=np.float32
+        )
 
         self.action_space = gym.spaces.Discrete(2)
+
+        self.terminated = True
 
     def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
 
         super().reset(seed=seed)
 
-        self.candidates = self.np_random.standard_normal(self.n, dtype=np.float32) * 15.0 + 50.0
-        orders = self.candidates.argsort()
+        self.scores = self.np_random.standard_normal(self.n, dtype=np.float32)
+
+        orders = self.scores.argsort()
         self.ranks = orders.argsort()
 
         self.candidate_idx = 0
+        self.terminated = False
 
-        observation = np.array([1.0, self.candidates[self.candidate_idx]], dtype=np.float32)
+        observation = np.array([1.0, self.scores[self.candidate_idx]], dtype=np.float32)
         info = {}
 
         return observation, info
 
     def step(self, action: int):
 
+        if self.terminated:
+
+            raise RuntimeError('step called on terminated environment')
+
+        if action not in (0, 1):
+
+            raise ValueError('action must be 0 or 1')
+
         if action == 0:
 
-            self.candidate_idx += 1
+            if self.candidate_idx < self.n - 1:
 
-            if self.candidate_idx < self.n:
+                self.candidate_idx += 1
 
-                observation = np.array([(self.n - self.candidate_idx) / self.n, self.candidates[self.candidate_idx]], dtype=np.float32)
+                observation = np.array([(self.n - self.candidate_idx) / self.n, self.scores[self.candidate_idx]], dtype=np.float32)
                 reward = 0.0
                 terminated = False
 
             # we have rejected all candidates and will forever be single :(
             else:
 
-                observation = np.array([0.0, 0.0], dtype=np.float32)
+                observation = np.array([0.0, self.scores[-1]], dtype=np.float32)
                 reward = -1.0
                 terminated = True
 
         else:
 
-            observation = np.array([0.0, 0.0], dtype=np.float32)
+            observation = np.array([0.0, self.scores[self.candidate_idx]], dtype=np.float32)
             # +1 because we are 0 indexed and we give some > 0 reward for any candidate (even the worst)
             reward = self.ranks[self.candidate_idx] + 1
             terminated = True
+
+        self.terminated = terminated
 
         truncated = False
         info = {}
--- test_env.py
+++ test_env.py
@@ -1,36 +1,45 @@
 """
-TODO
+test_env.py
+
+This script outputs states from an environment to allow a qualitative test.
 """
 
 import numpy as np
 
 from datingrl.env import RealScoreEnv
 
-env = RealScoreEnv()
+env = RealScoreEnv({})
 
+print('Observation Space:')
 print(env.observation_space)
+print('\nAction Space:')
 print(env.action_space)
 
 env.reset(0)
 assert env.ranks.min() == 0
 assert env.ranks.max() == 999
 
-print('Testing reject all:')
-print(env.reset(0))
+print('\nTesting reject all:')
+obs, info = env.reset(0)
+print(0, *obs, info)
 
-for _ in range(1000):
+for i in range(1, 1001):
 
-	print(env.step(0))
+	obs, *rest = env.step(0)
+	print(i, *obs, rest)
 
-print('Testing commits:')
-for i in range(10):
+print('\nTesting commits:')
+for i in range(1, 11):
 
 	env.reset(0)
 
-	for _ in range((i + 1) * 100 - 1):
+	# -1 so we can commit on the last step
+	for _ in range(i * 100 - 1):
 
 		observation, _, terminated, _, _ = env.step(0)
 		assert not terminated
 
-	print(env.step(1))
-	print(np.sum(env.candidates < observation[1]))
+	obs, *rest = env.step(1)
+	# the observation's "value" should match with the jth reject printed above
+	print(i * 100 - 1, *obs, rest)
+	print(np.sum(env.scores < observation[1]))
